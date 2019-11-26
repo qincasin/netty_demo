6d9f6f41514b160256da004bcc678c5df3f97d74
qincasin <qincasin@163.com>
2019-11-26 23:11:27 +0800

    feat : selector 网络编程

---
+++ src/main/java/com/qjx/nio/NioTest12.java
@@ -0,0 +1,22 @@
+package com.qjx.nio;
+
+import java.nio.channels.Selector;
+
+/**
+ * selector
+ */
+public class NioTest12 {
+    /**
+     * 服务器端监听五个端口号，每个端口号 都可以被客户端发起连接，这里使用一个线程处理所有连接
+     * @param args
+     */
+    public static void main(String[] args) throws Exception{
+        Selector selector = Selector.open();
+
+
+
+//        System.out.println(SelectorProvider.provider().getClass());
+//        System.out.println(sun.nio.ch.DefaultSelectorProvider.create().getClass());
+
+    }
+}
