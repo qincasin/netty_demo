1acb040f968461d92bb993bde94b48f554f645cd
qincasin <qincasin@163.com>
2020-01-07 21:58:59 +0800

    feat: 一个线程处理服务器多个端口和客户端的链接与通信

--- src/main/java/com/qjx/nio/NioTest12.java
+++ src/main/java/com/qjx/nio/NioTest12.java
@@ -1,7 +1,14 @@
 package com.qjx.nio;
 
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
-import java.nio.channels.spi.SelectorProvider;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * selector
@@ -9,14 +16,87 @@ import java.nio.channels.spi.SelectorProvider;
 public class NioTest12 {
     /**
      * 服务器端监听五个端口号，每个端口号 都可以被客户端发起连接，这里使用一个线程处理所有连接
+     *
      * @param args
      */
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
+        int[] ports = new int[5];
+        ports[0] = 5000;
+        ports[1] = 5001;
+        ports[2] = 5002;
+        ports[3] = 5003;
+        ports[4] = 5004;
+
+
         Selector selector = Selector.open();
+        for (int i = 0; i < ports.length; i++) {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            //设置成非阻塞模式
+            serverSocketChannel.configureBlocking(false);
+            ServerSocket serverSocket = serverSocketChannel.socket();
+            InetSocketAddress address = new InetSocketAddress(ports[i]);
+            serverSocket.bind(address);
+
+            //register
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            System.out.println("监听端口:" + ports[i]);
+        }
 
 
-        System.out.println(SelectorProvider.provider().getClass());
-        System.out.println(sun.nio.ch.DefaultSelectorProvider.create().getClass());
+        while (true) {
+            int numbers = selector.select();
+            System.out.println("numbers:" + numbers);
+
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = iterator.next();
+                //是否是可获取的
+                if (selectionKey.isAcceptable()) {
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+
+                    iterator.remove();
+
+                    System.out.println("获得客户端链接：" + socketChannel);
+                } else if (selectionKey.isReadable()) {
+                    System.out.println("is Readable ...");
+                    //是否可读的
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+
+                    int byteRead = 0;
+
+                    while (true) {
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+                        //首先需要clear掉
+                        byteBuffer.clear();
+                        int read = socketChannel.read(byteBuffer);
+                        if (read <= 0) {
+                            break;
+                        }
+                        byteBuffer.flip();
+
+                        //双向的 可读可写
+                        socketChannel.write(byteBuffer);
+
+                        byteRead += read;
+                    }
+
+                    System.out.println("读取：" + byteRead + "，来自于:" + socketChannel);
+
+                    iterator.remove();
+                }
+            }
+
+        }
+
+
+//        System.out.println(SelectorProvider.provider().getClass());
+//        System.out.println(sun.nio.ch.DefaultSelectorProvider.create().getClass());
 
     }
 }
